02a9702692933abd99e2df968fa6b032fdff6fe3
Kimmmking <434407818@qq.com>
2019-11-15 12:21:34 +0800

    合并了baidu的文字识别api

--- src/main/java/com/example/question_bank/web/QuestionController.java
+++ src/main/java/com/example/question_bank/web/QuestionController.java
@@ -3,12 +3,21 @@ package com.example.question_bank.web;
 import com.example.question_bank.pojo.Question;
 import com.example.question_bank.service.PropertyValueService;
 import com.example.question_bank.service.QuestionService;
+import com.example.question_bank.util.ImageUtil;
 import com.example.question_bank.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RestController
@@ -57,7 +66,54 @@ public class QuestionController {
         return questionService.listexcersice(uid);
     }
 
+//    图片文字识别API + 找题
+    @PostMapping("/search/picture")
+    public String searchByPic(MultipartFile image, HttpServletRequest request){
+        String folder = "img/";
+        File imageFolder= new File(request.getServletContext().getRealPath(folder));
+        File file = new File(imageFolder,image.getName()+".jpg");
 
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        try {
+            image.transferTo(file);
+            BufferedImage img = ImageUtil.change2jpg(file);
+            ImageIO.write(img, "jpg", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String imagePath = file.getAbsolutePath();
+        System.out.println(imagePath);
+        File apiFolder= new File(request.getServletContext().getRealPath("api/"));
+        String apiPath = apiFolder.getAbsolutePath() + "\\baiduapi.py";
+        System.out.println(apiPath);
+
+        String[] arguments = new String[] {"python", apiPath , imagePath};
+
+        String content = null;
+
+        try {
+            Process process = Runtime.getRuntime().exec(arguments);
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(),"GBK"));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                content += line + '\n';
+                System.out.println(line);
+            }
+            in.close();
+            return content;
+
+            //java代码中的process.waitFor()返回值为0表示我们调用python脚本成功，
+            //返回值为1表示调用python脚本失败，这和我们通常意义上见到的0与1定义正好相反
+//            int re = process.waitFor();
+//            System.out.println(re);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
 }
