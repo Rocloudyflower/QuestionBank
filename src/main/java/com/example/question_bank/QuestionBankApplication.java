0e38dfade11155d6778524aa80b66a721962117b
Kimmmking <434407818@qq.com>
2019-11-29 22:48:19 +0800

    完成了“排行榜”功能。注释“jieba” + “es”改成“sql”。

--- src/main/java/com/example/question_bank/QuestionBankApplication.java
+++ src/main/java/com/example/question_bank/QuestionBankApplication.java
@@ -7,14 +7,14 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableElasticsearchRepositories(basePackages = "com.example.question_bank.es")
+//@EnableElasticsearchRepositories(basePackages = "com.example.question_bank.es")
 @EnableJpaRepositories(basePackages = {"com.example.question_bank.dao","com.example.question_bank.pojo"})
 public class QuestionBankApplication {
 
-    static {
-        PortUtil.checkPort(9300,"ElasticSearch",true);
-        PortUtil.checkPort(5601,"Kibana可视化工具",true);
-    }
+//    static {
+//        PortUtil.checkPort(9300,"ElasticSearch",true);
+//        PortUtil.checkPort(5601,"Kibana可视化工具",true);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(QuestionBankApplication.class, args);
